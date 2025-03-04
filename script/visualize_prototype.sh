189cfd44639840dac3c8e490b095f1e641c23489
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-03-04 18:18:23 +0100

    added shapes

--- script/visualize_prototype.sh
+++ script/visualize_prototype.sh
@@ -1,3 +1,4 @@
+export CUDA_LAUNCH_BLOCKING=1
 python extract_prototypes.py --case_name Shapes_test --device 1 --model ResNet --basic_model resnet50_relu --concept_per_layer 8 16 32 64 --cha 32 32 32 32
 python ./vis_utils/find_topk_response.py --case_name Shapes_test --model ResNet --basic_model resnet50_relu --concept_per_layer 8 16 32 64 --cha 32 32 32 32 --device 1 --eigen_topk 1
 python ./vis_utils/find_topk_area.py --case_name Shapes_test --model ResNet --basic_model resnet50_relu --concept_per_layer 8 16 32 64 --cha 32 32 32 32 --topk 5 --device 1 --eigen_topk 1 --masked --heatmap --individually
